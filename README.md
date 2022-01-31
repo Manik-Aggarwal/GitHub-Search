4765c1a06152f3e3ff2a60f94600a0da10369401
Manik Aggarwal <manikaggarwal993@gmail.com>
2022-01-31 17:03:05 +0530

    Files added

---
+++ README.md
@@ -0,0 +1,5 @@
+# ->GitHub Search
+
+A simple web-app to search and display GitHub User's Profile using GitHub public APIs.
+
+Enter the username you want to search and click on the search button.
---
+++ app.js
@@ -0,0 +1,127 @@
+const url = `https://api.github.com/users`;
+
+// Displaying error if user not found
+const errorMessage = (status) => {
+    const messageDiv = document.querySelector("#message");
+    let errmsg = ``;
+    if (status === 404) {
+      errmsg = `<div class="alert alert-danger text-center">Profile doesn't exist.</div>`;
+    }
+    messageDiv.innerHTML = errmsg;
+    setTimeout(() => (messageDiv.innerHTML = ``), 3000);
+};
+
+// Fetching user profile
+const getProfile = async (login) => {
+  try {
+      const response = await fetch(`${url}/${login}`);
+      if (response.status !== 200) {
+          if (response.status === 404) errorMessage(response.status);
+          new Error(`Something went wrong! Status Code: ${response.status}`);
+      }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Fetching user repositories
+const getRepo = async (login) => {
+    try {
+        const response = await fetch(`${url}/${login}/repos`);
+        if (response.status !== 200) {
+          new Error(`Something went wrong! Status Code: ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        console.log(error);
+      }
+};
+
+// rendering user profile
+const renderProfile = (data) => {
+  let profileSnnipet = ``;
+  profileSnnipet += `<div class="profile-userpic">
+                    <img src="${data.avatar_url}" class="d-block">
+                    </div>
+                    <div class="profile-usertitle">`;
+  if (data.name !== null) {
+    profileSnnipet += `<div class="profile-usertitle-name">${data.name}</div>`;
+  }
+  profileSnnipet += `<div class="profile-usertitle-job">
+                    ${data.login}
+                    </div>
+                    </div>   
+                    <div class="portlet light bordered">
+                    <!-- STAT -->
+                    <div class="row list-separated profile-stat">
+                    <div class="col-md-6 col-sm-6 col-xs-6">
+                        <div class="uppercase profile-stat-title">${data.followers}</div>
+                        <div class="uppercase profile-stat-text"> Followers </div>
+                    </div>
+                    <div class="col-md-6 col-sm-6 col-xs-6">
+                        <div class="uppercase profile-stat-title">${data.following}</div>
+                        <div class="uppercase profile-stat-text"> Following </div>
+                    </div>
+                    </div>`;
+  if (data.bio !== null) {
+    profileSnnipet += `<div><h4 class="profile-desc-title">About ${data.name}</h4>
+                    <span class="profile-desc-text">${data.bio}</span></div>`;
+  }
+  if (data.twitter_username !== null) {
+    profileSnnipet += `<div class="margin-top-20 profile-desc-link">
+                    <i class="fab fa-twitter"></i>
+                    <a target="_blank" href="https://www.twitter.com/${data.twitter_username}">@${data.twitter_username}</a>
+                    </div>`;
+  }
+  profileSnnipet += `</div>`;
+  document.querySelector("#profile").innerHTML = profileSnnipet;
+};
+
+// rendering user repositories
+const renderRepo = (repos) => {
+  let reposList = ``;
+  if (repos.length > 0) {
+    repos.forEach((repo) => {
+      reposList += `<li class="mb-3 d-flex flex-content-stretch col-12 col-md-6 col-lg-6">
+        <div class="card" style="width: 22.5rem;">
+            <div class="card-body">
+            <h5 class="card-title"><a target="_blank" href="${repo.html_url}">${repo.name}</a></h5>
+            <p class="card-text">${
+              repo.description !== null ? repo.description : ""
+            }</p>
+            <p>`;
+      if (repo.language !== null) {
+        reposList += `<i class="fas fa-circle ${repo.language ? repo.language.toLowerCase() : ""}"></i> ${repo.language}`;
+      }
+      reposList += `<i class="far fa-star"></i> ${repo.stargazers_count}</p>
+            </div>
+        </div>
+      </li>`;
+    });
+  }
+  document.querySelector("#repos").innerHTML = reposList;
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const searchForm = document.querySelector("#searchForm");
+
+  searchForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const username = document.querySelector("#searchInput").value.trim();
+
+    if (username.length > 0) {
+      const userProfile = await getProfile(username);
+      if (userProfile.login) {
+        const userRepo = await getRepo(username);
+        renderProfile(userProfile);
+        renderRepo(userRepo);
+
+        document.querySelector(".searchblock").style.display = "none";
+        document.querySelector(".profile").style.display = "block";
+      }
+    }
+  });
+});
